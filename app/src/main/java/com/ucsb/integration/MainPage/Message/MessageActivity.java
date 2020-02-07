10d3028dad55e4d5fb49509e40bd2fd5a4565ec5
kylestubbs1234 <43654586+kylestubbs1234@users.noreply.github.com>
2020-02-06 18:19:02 -0800

    Update MessageActivity.java

--- app/src/main/java/com/ucsb/integration/MainPage/Message/MessageActivity.java
+++ app/src/main/java/com/ucsb/integration/MainPage/Message/MessageActivity.java
@@ -1,86 +1,44 @@
 package com.ucsb.integration.MainPage.Message;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.ucsb.integration.R;
 
-import java.util.ArrayList;
+public class MessageActivity extends Activity {
 
-public class MessageActivity extends AppCompatActivity {
-    private static final String TAG = "MessageActivity";
-
-    private ArrayList<String> mNames = new ArrayList<>();
-    private ArrayList<String> mImageUrls = new ArrayList<>();
-    private Context mContext;
-
-    @Override
+    Context context;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_message);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        Log.d(TAG, "Message Activity Started.");
-
-        initImageBitmaps();
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        setContentView(R.layout.activity_main);
+        context = this;
+        ListView lstview = (ListView) findViewById(R.id.listv);
+        lstview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(context, "An item of the ListView is clicked.", Toast.LENGTH_LONG).show();
             }
         });
+
+        String[] items = {"Kyle", "Stubbs", "Marisa", "Bob", "Naomi"};
+        LstViewAdapter adapter = new LstViewAdapter(this, R.layout.list_item, R.id.txt, items);
+        // Bind data to the ListView
+        lstview.setAdapter(adapter);
     }
 
-    private void initImageBitmaps() {
-        Log.d(TAG, "initImageBitmaps: preparing bitmaps.");
-
-        mImageUrls.add("https://c1/staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg");
-        mNames.add("Havasu Falls");
-
-        mImageUrls.add("https://i.redd.it/tpsnoz5bzo501.jpg");
-        mNames.add("Trondheim");
-
-        mImageUrls.add("https://i.redd.it/qn7f9oqu7o501.jpg");
-        mNames.add("Portugal");
-
-        mImageUrls.add("https://i.redd.it/j6myfqglup501.jpg");
-        mNames.add("Rocky Mountain National Park");
-
-        mImageUrls.add("https://i.redd.it/oh2gmlix6p501.jpg");
-        mNames.add("Mahahual");
-
-        mImageUrls.add("https://i.redd.it/k98uz168ch501.jpg");
-        mNames.add("Frozen Lake");
-
-        mImageUrls.add("https://i.redd.it/glin0nwndo501.jpg");
-        mNames.add("Frozen Lake");
-
-        mImageUrls.add("https://i.redd.it/obx4zvdshg601.jpg");
-        mNames.add("Australia");
-
-        mImageUrls.add("https://i.imgur.com/ZcLLrkY.jpg");
-        mNames.add("Canada");
-
-        initRecyclerView();
+    public void clickMe(View view){
+        Button bt=(Button)view;
+        Toast.makeText(this, "Button "+bt.getText().toString(),Toast.LENGTH_LONG).show();
     }
 
-    private void initRecyclerView() {
-        Log.d(TAG, "initRecyclerView: init recyclerview.");
-        RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(mImageUrls, mNames, mContext);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-    }
 }
