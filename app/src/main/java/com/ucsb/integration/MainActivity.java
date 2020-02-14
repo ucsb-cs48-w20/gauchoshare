8d9a3dca82bc034de285a8f1a8dd0429caf56d1d
Christopher Yang <christopheryang@Christophers-MacBook-Pro.local>
2020-02-13 17:21:05 -0800

    profile display works now

--- app/src/main/java/com/ucsb/integration/MainActivity.java
+++ app/src/main/java/com/ucsb/integration/MainActivity.java
@@ -5,10 +5,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.ucsb.integration.MainPage.Find.FindActivity;
 import com.ucsb.integration.MainPage.Message.MessageActivity;
+import com.ucsb.integration.MainPage.Profile.ProfileActivity;
 import com.ucsb.integration.MainPage.Profile.SetupActivity;
 import com.ucsb.integration.MainPage.Share.ShareActivity;
 
@@ -18,11 +26,20 @@ public class MainActivity extends AppCompatActivity {
     Button share;
     Button message;
     Button profile;
+    private FirebaseAuth mAuth;
+    private DatabaseReference UsersRef;
+    private FirebaseDatabase database;
 
+    String currentUserID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        database = FirebaseDatabase.getInstance();
+        UsersRef = database.getReference().child("Users");
 
         find = findViewById(R.id.find);
         find.setOnClickListener(new View.OnClickListener() {
@@ -52,8 +69,22 @@ public class MainActivity extends AppCompatActivity {
         profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Replace SetupActivity.class with ProfileActivity.class once bug is resolved
-                startActivity(new Intent(MainActivity.this, SetupActivity.class));
+                UsersRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (!dataSnapshot.hasChild(currentUserID)) {
+                            startActivity(new Intent(MainActivity.this, SetupActivity.class));
+
+                        } else {
+                            startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
             }
         });
     }
--- app/src/main/java/com/ucsb/integration/MainPage/Profile/ProfileActivity.java
+++ app/src/main/java/com/ucsb/integration/MainPage/Profile/ProfileActivity.java
@@ -1,20 +1,64 @@
 package com.ucsb.integration.MainPage.Profile;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.ucsb.integration.R;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ProfileActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private DatabaseReference UsersRef;
+    private FirebaseDatabase database;
+    String currentUserID;
+    Map<String, Object> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        data = new HashMap<>();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        database = FirebaseDatabase.getInstance();
+        UsersRef = database.getReference().child("Users").child(currentUserID);
+
+        final TextView userNameView = (TextView) findViewById(R.id.username);
+        final TextView fullNameView = (TextView) findViewById(R.id.full_name);
+        final TextView emailView = (TextView) findViewById(R.id.email);
+        final TextView phoneNumberView = (TextView) findViewById(R.id.phone_number);
+
+        UsersRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                data = (Map<String, Object>) dataSnapshot.getValue();
+                    userNameView.setText(data.get("username").toString());
+                    fullNameView.setText(data.get("fullname").toString());
+                    emailView.setText(data.get("email").toString());
+                    phoneNumberView.setText(data.get("phonenumber").toString());
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
-
 }
