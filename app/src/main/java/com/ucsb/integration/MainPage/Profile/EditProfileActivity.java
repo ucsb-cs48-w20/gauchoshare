3fc94e6589c2467eeb28321aead12f006a30ca15
Christopher Yang <christopheryang@Christophers-MacBook-Pro.local>
2020-03-04 00:14:55 -0800

    first commit

---
+++ app/src/main/java/com/ucsb/integration/MainPage/Profile/EditProfileActivity.java
@@ -0,0 +1,16 @@
+package com.ucsb.integration.MainPage.Profile;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ucsb.integration.R;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
