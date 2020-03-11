b60360b0d151084679c3ad9a269b6756669d6346
Kyle Stubbs <kylestubbs@ucsb.edu>
2020-03-10 23:31:03 -0700

    able to sort users lists

--- app/src/main/java/com/ucsb/integration/Fragments/ChatsFragment.java
+++ app/src/main/java/com/ucsb/integration/Fragments/ChatsFragment.java
@@ -24,6 +24,7 @@ import com.ucsb.integration.R;
 import com.ucsb.integration.adapter.UserAdapter;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.ListIterator;
 
@@ -107,6 +108,7 @@ public class ChatsFragment extends Fragment {
                         }
                     }
                 }
+                Collections.sort(mUsers);
                 userAdapter = new UserAdapter(getContext(), mUsers);
                 recyclerView.setAdapter(userAdapter);
             }
